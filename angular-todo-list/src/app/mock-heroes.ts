87d840835f31cae6e0f3978df6d013b2b9ce43a8
Dipen Ved <vedipen@gmail.com>
2018-04-14 19:43:19 +0530

    Make todo list

---
+++ angular-todo-list/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Complete todo' },
+  { id: 12, name: 'Learn Angular' },
+  { id: 13, name: 'Todo Item 3' },
+  { id: 14, name: 'Todo Item 4' },
+  { id: 15, name: 'Todo Item 5' },
+  { id: 16, name: 'Todo Item 6' },
+  { id: 17, name: 'Todo Item 7' },
+  { id: 18, name: 'Todo Item 8' },
+  { id: 19, name: 'Todo Item 9' },
+  { id: 20, name: 'Todo Item 10' }
+];
